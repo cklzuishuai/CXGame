2ad0fcaed4eefbd687ff76ad75e7ca4ceec6b9f1
ckl <ckl@DESKTOP-MKDOBGO>
2019-02-13 22:03:17 +0800

    1

--- myproject/src/main/java/com/myproject/WebSocket/MyHandler.java
+++ myproject/src/main/java/com/myproject/WebSocket/MyHandler.java
@@ -15,6 +15,8 @@ import javax.websocket.server.ServerEndpoint;
 
 import org.springframework.stereotype.Component;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.myproject.Game.Game;
 
 @ServerEndpoint("/community/{ro_user}")
@@ -26,13 +28,14 @@ public class MyHandler {
 	//存放房间对应的牌局
 	private static final Map<Integer, Game> room_game = new HashMap<Integer, Game>();
 	private Session session; //Socket会话
-	private Integer userId;  //用户ID
-	private Integer roomId;  //房间ID
+	private String userId;  //用户ID
+	private Integer roomId = 1;  //房间ID
 	
 	@OnOpen
 	public void onOpen(@PathParam(value = "ro_user") String ro_user, Session session) {
 		System.out.println(ro_user+"建立socket连接");
 		this.session = session;
+		this.userId = ro_user;
 		CopyOnWriteArraySet<MyHandler> players = room_user.get(roomId);
 		if (players == null) {
 			synchronized (room_user) {
@@ -43,6 +46,7 @@ public class MyHandler {
 			}
 		}
 		players.add(this);
+		//System.out.println(players);
 		if(players.size() == 4){
 			initGame();
 		}
@@ -51,6 +55,11 @@ public class MyHandler {
 	@OnClose    
 	public void onClose() {
 		System.out.println("有一连接关闭");
+		CopyOnWriteArraySet<MyHandler> players = room_user.get(roomId);
+	    if (players != null) {
+	    	  players.remove(this);
+	    }
+	    //System.out.println(players);
 	}
 	
     @OnError    
@@ -61,12 +70,41 @@ public class MyHandler {
     
     @OnMessage
     public void onMessage(final String message, Session session) {
-    	
+    	Game game = room_game.get(roomId);
+    	JSONObject jso = JSON.parseObject(message);
+    	if(jso.getString("type").equals("play")){
+    		game.playRound(userId,jso.getInteger("index"));
+    		showPrivateHave();
+    	}
+    }
+    
+    private void showPrivateHave(){
+    	Game game = room_game.get(roomId);    	
+    	this.session.getAsyncRemote().sendText(game.getPlayerHave(this.userId));
     }
     
     //牌局初始化
     public void initGame(){
-    	
+    	Game game = new Game();
+    	room_game.put(roomId, game);
+    	System.out.println("牌局初始化");
+    	game.init();
+        CopyOnWriteArraySet<MyHandler> players = room_user.get(roomId);        
+        if (players != null) {            
+        	for (MyHandler item : players) { 
+        		item.session.getAsyncRemote().sendText(game.getPlayerHave(item.userId));
+        	}
+        	for (MyHandler item : players) {                
+        		item.session.getAsyncRemote().sendText(game.getKing());
+        	} 
+        	for (MyHandler item : players) {    
+        		if(item.userId.equals(game.getFirst())){
+        			item.session.getAsyncRemote().sendText("play");
+        		}
+        	}
+        }
+
     }
+    
 
 }
